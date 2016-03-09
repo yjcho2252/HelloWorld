a5a9c828194891fd078b6b2e998d7081f79548e6
yjcho2252 <yongjoon@chunlab.com>
2016-03-09 21:43:01 +0900

    ini

--- hello.py
+++ hello.py
@@ -1,1 +1,6 @@
-print("Hello World!")
+import rpy2.robjects as robjects
+read_delim = robjects.r('read.delim')
+seq_data = read_delim('sequence.index', header=True, stringsAsFactors=False)
+
+print('This dataframe has %d columns and %d rows' %(seq_data.ncol, seq_data.nrow))
+print(seq_data.colnames)
